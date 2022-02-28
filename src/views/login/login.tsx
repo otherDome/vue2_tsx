f9c4a4fee3002645edf9bafb0f791e2f9e2f047d
北斋 <1038886097@qq.com>
2022-02-28 19:35:56 +0800

    技术架构调整

--- src/views/login/login.tsx
+++ src/views/login/login.tsx
@@ -5,14 +5,25 @@ import style from '@/views/login/style/login.module.scss';
 @Component
 export default class App extends Vue {
   private logs() {
-    console.log("aaa")
-    this.$router.push({
-      path: '/index'
-    });
+    console.log("aaa", this.nakcname)
+    // this.$router.push({
+    //   path: '/index'
+    // });
   }
+  private elProps: any = {
+    name: ""
+  }
+  private nakcname: string = ''
   protected render() {
     return <div class={style.login}>
       这是登录
+      <el-form ref="form" props={{
+        model: this.elProps
+      }} label-width="80px">
+        <el-form-item label="活动名称">
+          <el-input v-model={this.nakcname} placeholder="请输入内容"></el-input>
+        </el-form-item>
+      </el-form>
       <el-button type="danger" onClick={this.logs.bind(this)}>危险按钮</el-button>
     </div>;
   }
