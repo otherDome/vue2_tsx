476da5fa70efb7d8da1ce77d8e328b936eaab6a5
北斋 <1038886097@qq.com>
2022-04-29 18:08:04 +0800

    随访记录

--- src/views/DataReport/followup.tsx
+++ src/views/DataReport/followup.tsx
@@ -33,6 +33,501 @@ export default class followup extends Mixins(onresize) {
   protected isvalue2: string = ''
   protected screening: boolean = false
 
+  //表格设置数据变量
+  protected elTabletsxborder: boolean = true
+  protected elTabletsxstripe: boolean = true
+  protected elTabletsxsize: String = 'mini'
+  protected elTabletsxcellStyle: string = 'height:28px;padding:0px;font-size:13px;line-height: 28px;text-align: center;'
+  protected elTabletsxheadercellstyle: string = 'height:28px;padding:0px;text-align: center;'
+  protected elTabletsxoddnumber: string = 'background: rgb(238 250 255 / 80%);'
+  protected elTabletsxevennumber: string = 'background: rgb(238 250 255 / 30%);'
+  protected elTabletsxheaderBGS: string = "background: linear-gradient(to bottom, #EEF5FB 0%,#E2EDFA 100%);"
+
+  protected tableData: any = [
+    {
+      id: "12987122",
+      name: "王小虎",
+      amount1: "234",
+      amount2: "3.2",
+      amount3: 10,
+      amount4: "3.2",
+      amount5: "3.2",
+      amount6: "3.2",
+      amount7: "3.2",
+      amount8: "3.2",
+      amount9: "3.2",
+      amount10: "-3.2",
+      amount11: "983.2",
+      amount12: "-3.2",
+      amount13: "53.2",
+    },
+    {
+      id: "12987122",
+      name: "王小虎",
+      amount1: "234",
+      amount2: "3.2",
+      amount3: 10,
+      amount4: "3.2",
+      amount5: "3.2",
+      amount6: "3.2",
+      amount7: "3.2",
+      amount8: "3.2",
+      amount9: "3.2",
+      amount10: "-3.2",
+      amount11: "983.2",
+      amount12: "-3.2",
+      amount13: "53.2",
+    },
+    {
+      id: "12987122",
+      name: "王小虎",
+      amount1: "234",
+      amount2: "3.2",
+      amount3: 10,
+      amount4: "3.2",
+      amount5: "3.2",
+      amount6: "3.2",
+      amount7: "3.2",
+      amount8: "3.2",
+      amount9: "3.2",
+      amount10: "-3.2",
+      amount11: "983.2",
+      amount12: "-3.2",
+      amount13: "53.2",
+    },
+    {
+      id: "12987122",
+      name: "王小虎",
+      amount1: "234",
+      amount2: "3.2",
+      amount3: 10,
+      amount4: "3.2",
+      amount5: "3.2",
+      amount6: "3.2",
+      amount7: "3.2",
+      amount8: "3.2",
+      amount9: "3.2",
+      amount10: "-3.2",
+      amount11: "983.2",
+      amount12: "-3.2",
+      amount13: "53.2",
+    },
+    {
+      id: "12987123",
+      name: "王小虎",
+      amount1: "165",
+      amount2: "4.43",
+      amount3: 12,
+      amount4: "3.2",
+      amount5: "3.2",
+      amount6: "3.2",
+      amount7: "3.2",
+      amount8: "3.2",
+      amount9: "3.2",
+      amount10: "-3.2",
+      amount11: "983.2",
+      amount12: "-3.2",
+      amount13: "53.2",
+    },
+    {
+      id: "12987124",
+      name: "王小虎",
+      amount1: "324",
+      amount2: "1.9",
+      amount3: 9,
+      amount4: "3.2",
+      amount5: "3.2",
+      amount6: "3.2",
+      amount7: "3.2",
+      amount8: "3.2",
+      amount9: "3.2",
+      amount10: "-3.2",
+      amount11: "983.2",
+      amount12: "-3.2",
+      amount13: "53.2",
+    },
+    {
+      id: "12987125",
+      name: "王小虎",
+      amount1: "621",
+      amount2: "2.2",
+      amount3: 17,
+      amount4: "3.2",
+      amount5: "3.2",
+      amount6: "3.2",
+      amount7: "3.2",
+      amount8: "3.2",
+      amount9: "3.2",
+      amount10: "-3.2",
+      amount11: "983.2",
+      amount12: "-3.2",
+      amount13: "53.2",
+    },
+    {
+      id: "12987126",
+      name: "王小虎",
+      amount1: "539",
+      amount2: "4.1",
+      amount3: 15,
+      amount4: "3.2",
+      amount5: "3.2",
+      amount6: "3.2",
+      amount7: "3.2",
+      amount8: "3.2",
+      amount9: "3.2",
+      amount10: "-3.2",
+      amount11: "983.2",
+      amount12: "-3.2",
+      amount13: "53.2",
+    },
+    {
+      id: "12987126",
+      name: "王小虎",
+      amount1: "539",
+      amount2: "4.1",
+      amount3: 15,
+      amount4: "3.2",
+      amount5: "3.2",
+      amount6: "3.2",
+      amount7: "3.2",
+      amount8: "3.2",
+      amount9: "3.2",
+      amount10: "-3.2",
+      amount11: "983.2",
+      amount12: "-3.2",
+      amount13: "53.2",
+    },
+    {
+      id: "12987126",
+      name: "王小虎",
+      amount1: "539",
+      amount2: "4.1",
+      amount3: 15,
+      amount4: "3.2",
+      amount5: "3.2",
+      amount6: "3.2",
+      amount7: "3.2",
+      amount8: "3.2",
+      amount9: "3.2",
+      amount10: "-3.2",
+      amount11: "983.2",
+      amount12: "-3.2",
+      amount13: "53.2",
+    },
+    {
+      id: "12987126",
+      name: "王小虎",
+      amount1: "539",
+      amount2: "4.1",
+      amount3: 15,
+      amount4: "3.2",
+      amount5: "3.2",
+      amount6: "3.2",
+      amount7: "3.2",
+      amount8: "3.2",
+      amount9: "3.2",
+      amount10: "-3.2",
+      amount11: "983.2",
+      amount12: "-3.2",
+      amount13: "53.2",
+    },
+    {
+      id: "12987126",
+      name: "王小虎",
+      amount1: "539",
+      amount2: "4.1",
+      amount3: 15,
+      amount4: "3.2",
+      amount5: "3.2",
+      amount6: "3.2",
+      amount7: "3.2",
+      amount8: "3.2",
+      amount9: "3.2",
+      amount10: "-3.2",
+      amount11: "983.2",
+      amount12: "-3.2",
+      amount13: "53.2",
+    },
+    {
+      id: "12987126",
+      name: "王小虎",
+      amount1: "539",
+      amount2: "4.1",
+      amount3: 15,
+      amount4: "3.2",
+      amount5: "3.2",
+      amount6: "3.2",
+      amount7: "3.2",
+      amount8: "3.2",
+      amount9: "3.2",
+      amount10: "-3.2",
+      amount11: "983.2",
+      amount12: "-3.2",
+      amount13: "53.2",
+    },
+    {
+      id: "12987126",
+      name: "王小虎",
+      amount1: "539",
+      amount2: "4.1",
+      amount3: 15,
+      amount4: "3.2",
+      amount5: "3.2",
+      amount6: "3.2",
+      amount7: "3.2",
+      amount8: "3.2",
+      amount9: "3.2",
+      amount10: "-3.2",
+      amount11: "983.2",
+      amount12: "-3.2",
+      amount13: "53.2",
+    },
+    {
+      id: "12987126",
+      name: "王小虎",
+      amount1: "539",
+      amount2: "4.1",
+      amount3: 15,
+      amount4: "3.2",
+      amount5: "3.2",
+      amount6: "3.2",
+      amount7: "3.2",
+      amount8: "3.2",
+      amount9: "3.2",
+      amount10: "-3.2",
+      amount11: "983.2",
+      amount12: "-3.2",
+      amount13: "53.2",
+    },
+    {
+      id: "12987126",
+      name: "王小虎",
+      amount1: "539",
+      amount2: "4.1",
+      amount3: 15,
+      amount4: "3.2",
+      amount5: "3.2",
+      amount6: "3.2",
+      amount7: "3.2",
+      amount8: "3.2",
+      amount9: "3.2",
+      amount10: "-3.2",
+      amount11: "983.2",
+      amount12: "-3.2",
+      amount13: "53.2",
+    },
+    {
+      id: "12987126",
+      name: "王小虎",
+      amount1: "539",
+      amount2: "4.1",
+      amount3: 15,
+      amount4: "3.2",
+      amount5: "3.2",
+      amount6: "3.2",
+      amount7: "3.2",
+      amount8: "3.2",
+      amount9: "3.2",
+      amount10: "-3.2",
+      amount11: "983.2",
+      amount12: "-3.2",
+      amount13: "53.2",
+    },
+    {
+      id: "12987126",
+      name: "王小虎",
+      amount1: "539",
+      amount2: "4.1",
+      amount3: 15,
+      amount4: "3.2",
+      amount5: "3.2",
+      amount6: "3.2",
+      amount7: "3.2",
+      amount8: "3.2",
+      amount9: "3.2",
+      amount10: "-3.2",
+      amount11: "983.2",
+      amount12: "-3.2",
+      amount13: "53.2",
+    },
+    {
+      id: "12987126",
+      name: "王小虎",
+      amount1: "539",
+      amount2: "4.1",
+      amount3: 15,
+      amount4: "3.2",
+      amount5: "3.2",
+      amount6: "3.2",
+      amount7: "3.2",
+      amount8: "3.2",
+      amount9: "3.2",
+      amount10: "-3.2",
+      amount11: "983.2",
+      amount12: "-3.2",
+      amount13: "53.2",
+    },
+    {
+      id: "12987126",
+      name: "王小虎",
+      amount1: "539",
+      amount2: "4.1",
+      amount3: 15,
+      amount4: "3.2",
+      amount5: "3.2",
+      amount6: "3.2",
+      amount7: "3.2",
+      amount8: "3.2",
+      amount9: "3.2",
+      amount10: "-3.2",
+      amount11: "983.2",
+      amount12: "-3.2",
+      amount13: "53.2",
+    },
+  ];
+  protected elTableColumn: any = [
+    {
+      label: "ID",
+      prop: "id",
+      width: "100",
+      showOverflowTooltip: true,
+      showtableColumn: true
+    },
+    {
+      type: "Thecustom",
+      label: "姓名",
+      width: "85",
+      prop: "name",
+      showOverflowTooltip: true,
+      renderHeader: (h: any, { column }: any) => {
+        return [
+          column.label,
+          h(
+            "el-tooltip",
+            {
+              props: {
+                content: "这是提示文案123",
+                placement: "top",
+              },
+            },
+            [
+              h(
+                "span",
+                {
+                  class: "el-icon-question",
+                  style: "color:#409EFF;"
+                }
+              )
+            ]
+          ),
+        ];
+      },
+      showtableColumn: true
+    }, {
+      label: "参数1",
+      minwidth: '85',
+      prop: "amount1",
+      showOverflowTooltip: true,
+      showtableColumn: true
+    },
+    {
+      label: "参数2",
+      minwidth: '85',
+      sortable: true,
+      prop: "amount2",
+      showOverflowTooltip: true,
+      showtableColumn: true
+    },
+    {
+      label: "参数3",
+      minwidth: '85',
+      sortable: true,
+      prop: "amount3",
+      showOverflowTooltip: true,
+      showtableColumn: true
+    },
+    {
+      label: "参数4",
+      minwidth: '85',
+      sortable: true,
+      prop: "amount4",
+      showOverflowTooltip: true,
+      showtableColumn: true
+    },
+    {
+      label: "参数5",
+      minwidth: '85',
+      sortable: true,
+      prop: "amount5",
+      showOverflowTooltip: true,
+      showtableColumn: true
+    },
+    {
+      label: "参数5",
+      minwidth: '85',
+      sortable: true,
+      prop: "amount6",
+      showOverflowTooltip: true,
+      showtableColumn: true
+    },
+    {
+      label: "参数6",
+      minwidth: '85',
+      prop: "amount7",
+      showOverflowTooltip: true,
+      showtableColumn: true
+    },
+    {
+      label: "参数7",
+      minwidth: '85',
+      prop: "amount8",
+      showOverflowTooltip: true,
+      showtableColumn: true
+    },
+    {
+      label: "参数8",
+      minwidth: '85',
+      prop: "amount9",
+      showOverflowTooltip: true,
+      showtableColumn: true
+    },
+    {
+      label: "参数9",
+      minwidth: '85',
+      prop: "amount9",
+      showOverflowTooltip: true,
+      showtableColumn: true
+    },
+    {
+      label: "参数10",
+      minwidth: '95',
+      prop: "amount10",
+      showOverflowTooltip: true,
+      showtableColumn: true
+    },
+    {
+      label: "参数11",
+      minwidth: '95',
+      prop: "amount11",
+      showOverflowTooltip: true,
+      showtableColumn: true
+    },
+    {
+      label: "参数12",
+      minwidth: '95',
+      prop: "amount12",
+      showOverflowTooltip: true,
+      showtableColumn: true
+    },
+    {
+      label: "参数13",
+      minwidth: '95',
+      prop: "amount13",
+      showOverflowTooltip: true,
+      showtableColumn: true
+    }
+  ]
+
   protected onScreeningClk() {
     this.screening = !this.screening
     console.log('点击高级筛选')
@@ -45,7 +540,7 @@ export default class followup extends Mixins(onresize) {
   }
   protected render() {
     return <div class={style.followup}>
-      <div class={style.followup_from}>
+      <div class={style.followup_from} id='elFromwidth'>
         {
           this.screening === false ? <div class={style.followup_from_pt}>
             <div class={style.followup_from_pt_left}>
@@ -56,12 +551,18 @@ export default class followup extends Mixins(onresize) {
                 <el-form-item>
                   <el-input v-model={this.isvalue} size='mini' placeholder="请输入会员姓名"></el-input>
                 </el-form-item>
-                <el-form-item>
-                  <el-input v-model={this.isvalue} size='mini' placeholder="请输入回访人姓名"></el-input>
-                </el-form-item>
-                <el-form-item>
-                  <el-input v-model={this.isvalue} size='mini' placeholder="请输入会员手机号"></el-input>
-                </el-form-item>
+                {
+                  this.elFromwidth > 680 ? <el-form-item>
+                    <el-input v-model={this.isvalue} size='mini' placeholder="请输入回访人姓名"></el-input>
+                  </el-form-item> : ''
+                }
+
+                {
+                  this.elFromwidth > 850 ? <el-form-item>
+                    <el-input v-model={this.isvalue} size='mini' placeholder="请输入会员手机号"></el-input>
+                  </el-form-item> : ''
+                }
+
               </el-form>
             </div>
             <div class={style.followup_from_pt_right}>
@@ -153,7 +654,84 @@ export default class followup extends Mixins(onresize) {
         }
       </div>
       <div class={style.followup_Table}>
-        随访记录
+        <div class={style.followup_Table_header}>
+          <datalistTableTitle
+            props={{
+              tableColumn: this.elTableColumn
+            }}
+            {
+            ...{
+              on: {
+                'checkboxGroup': (item: any) => {
+                  this.elTabletsxstripe = item.includes('斑马线')
+                  this.elTabletsxborder = item.includes('边框')
+                  if (this.elTabletsxstripe) {
+                    this.elTabletsxoddnumber = 'background: rgb(238 250 255 / 80%);'
+                    this.elTabletsxevennumber = 'background: rgb(238 250 255 / 30%);'
+                    this.elTabletsxheaderBGS = "background: linear-gradient(to bottom, #EEF5FB 0%,#E2EDFA 100%);"
+                  } else {
+                    this.elTabletsxoddnumber = ''
+                    this.elTabletsxevennumber = ''
+                    this.elTabletsxheaderBGS = 'background:#F5F7FA;'
+                  }
+                },
+                'radioGroup': (item: any) => {
+                  if (item === 'mini') {
+                    this.elTabletsxcellStyle = 'height:22px;padding:0px;font-size:13px;line-height: 22px;text-align: center;'
+                    this.elTabletsxheadercellstyle = 'height:20px;padding:0px;text-align: center;'
+                  } else if (item === 'small' || item === 'medium') {
+                    this.elTabletsxcellStyle = 'text-align: center;'
+                    this.elTabletsxheadercellstyle = 'text-align: center;'
+                  }
+                  this.elTabletsxsize = item
+                },
+              }
+            }
+            }
+          >
+          </datalistTableTitle>
+        </div>
+        <div class={style.followup_Table_list}>
+          <elTabletsx
+            props={{
+              data: this.tableData,
+              border: this.elTabletsxborder,
+              stripe: this.elTabletsxstripe,
+              size: this.elTabletsxsize,
+              tooltipEffect: "dark",
+              serialnumber: true,
+              elTableColumn: this.elTableColumn,
+              stylecLIS: "border-radius: 5px;box-shadow: #ebeef5 0px 0px 13px;color:#515151;",
+              cellStyle: ({ row, rowIndex }: any) => {
+                var style = this.elTabletsxcellStyle
+                if (rowIndex % 2 == 0) {
+                  style += this.elTabletsxoddnumber
+                }
+                if (rowIndex % 2 !== 0) {
+                  style += this.elTabletsxevennumber
+                }
+                return style
+              },
+              headercellstyle: () => {
+                var style = this.elTabletsxheadercellstyle
+                style += this.elTabletsxheaderBGS
+                return style;
+              },
+              showpagination: true,
+              total: 10
+            }}
+            scopedSlots={{
+              Thecustom: (param: any) => {
+                const item: any = param.isitem.row
+                return <div>
+                  <i class="el-icon-time"></i>
+                  <span style="margin-left: 10px">{item.name}</span>
+                </div>
+              }
+            }}
+          >
+          </elTabletsx>
+        </div>
       </div>
 
     </div>
